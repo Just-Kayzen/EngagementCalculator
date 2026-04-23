1f60e6f517a67a3066b24b4cf9e84add573da9bc
Jake <jaketoledo77@gmail.com>
2026-04-23 15:56:12 +0800

    ExtractLink from GSheets

--- TestHome.py
+++ TestHome.py
@@ -3,8 +3,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 credsPath = r"C:\Python\EngagementCalculator\engagementcalculator-fef079d955ba.json"
-sheetName = r"ReadThis"
-
+sheetID = r"1_SMHZEbGrT8MmSgH7hFLA9DuQ2zbf3i5GMREYh3ZnDM"
 
 # Define the scope
 scope = [
@@ -15,12 +14,20 @@ scope = [
 
 # Add credentials
 creds = Credentials.from_service_account_file(credsPath, scopes=scope)
-
 # Authenticate and create the client
 client = gspread.authorize(creds)
 
-# Open the spreadsheet
-sheet = client.open(sheetName).sheet1
 
-sheet = client.open_by_key(r"1_SMHZEbGrT8MmSgH7hFLA9DuQ2zbf3i5GMREYh3ZnDM").sheet1
-print(sheet.get_all_records())
+sheet = client.open_by_key(sheetID).sheet1
+
+# Get all values in the sheet
+all_values = sheet.get_all_values()
+
+# Find the index of the "Youtube Links" column
+header = all_values[0]  # first row is the header
+col_index = header.index("Youtube Links")  # find the column position
+
+# Extract all values from that column, skipping the header
+youtube_links = [row[col_index] for row in all_values[1:]]
+
+print(youtube_links)
