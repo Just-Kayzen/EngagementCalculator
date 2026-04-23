cf3656b0ab621636ced720314d187f1552a820de
Jake <jaketoledo77@gmail.com>
2026-04-23 12:27:51 +0800

    try Working reading GSheets

--- TestHome.py
+++ TestHome.py
@@ -1,5 +1,26 @@
 import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
 
-googlesheet  = "https://docs.google.com/spreadsheets/d/1_SMHZEbGrT8MmSgH7hFLA9DuQ2zbf3i5GMREYh3ZnDM/export?format=csv&gid=0"
+credsPath = r"C:\Python\EngagementCalculator\engagementcalculator-fef079d955ba.json"
+sheetName = r"ReadThis"
 
-df = pd.read_csv(googlesheet)
+
+# Define the scope
+scope = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    "https://www.googleapis.com/auth/drive"
+]
+
+
+# Add credentials
+creds = Credentials.from_service_account_file(credsPath, scopes=scope)
+
+# Authenticate and create the client
+client = gspread.authorize(creds)
+
+# Open the spreadsheet
+sheet = client.open(sheetName).sheet1
+
+sheet = client.open_by_key(r"1_SMHZEbGrT8MmSgH7hFLA9DuQ2zbf3i5GMREYh3ZnDM").sheet1
+print(sheet.get_all_records())
